b143bb0e1c88f5f01a09e817a5fdf6dcb35df410
Ari Lamstein <arilamstein@gmail.com>
2023-12-26 12:37:10 -0800

    use day5 app instead of day1 app because it's more interesting

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,39 @@
+import numpy as np
+import altair as alt
+import pandas as pd
 import streamlit as st
 
-st.header('Button app!')
+st.header('st.write')
 
-if st.button('Say Hello'):
-    st.write('Hello')
-else:
-    st.write('Goodbye')
+# Example 1
+
+st.write('Hello, *World!* :sunglasses:')
+
+# Example 2
+
+st.write(1234)
+
+# Example 3
+
+df = pd.DataFrame({
+     'first column': [1, 2, 3, 4],
+     'second column': [10, 20, 30, 40]
+     })
+st.write(df)
+
+# Example 4
+
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+# Example 5
+
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+c = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
+
+# Example 6
+d = {'a': 1, 'b': 2, 3: 3}
+st.write(d)
